a2f6592594420c9f1b3950fde25eb11132e00046
Tabslate <tabslate@localhost>
2026-08-17 16:10:28 -0400

    Share nl-interface

--- assets/Title-B0HGUr3s.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CCICIYih.js";import{bp as r}from"./tabs-CATyAcZ6.js";function n({className:t,children:e,...s}){return o.jsx("h1",{className:r("text-base font-bold flex items-center justify-between mb-4",t),...s,children:e})}export{n as T};
