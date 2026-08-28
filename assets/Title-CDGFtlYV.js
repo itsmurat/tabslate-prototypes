d5e91a680a6d2182a5ff8a1e2b939468764278bb
Tabslate <tabslate@localhost>
2026-08-28 18:42:36 -0400

    Share sticky-header-behavior-pages

--- assets/Title-CDGFtlYV.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-B0pWrypq.js";import{bA as r}from"./tabs-DosTIkz7.js";function n({className:t,children:e,...s}){return o.jsx("h1",{className:r("text-base font-bold flex items-center justify-between mb-4",t),...s,children:e})}export{n as T};
