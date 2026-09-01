44eabe2ebb601d9bed0ece0f872209128abec35f
Tabslate <tabslate@localhost>
2026-09-01 19:09:35 -0400

    Share nl-interface

---
+++ assets/ellipsis-rAjd37Zk.js
@@ -0,0 +1,16 @@
+import{c}from"./tabs-QgL7pGgd.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]],s=c("arrow-up",e);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=c("chevron-left",o);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]],n=c("ellipsis",r);export{s as A,t as C,n as E};
